2a54b51b5d734cd39b5494f2e5e0206ccc73c509
Mikhail <mvnorkin@gmail.com>
2022-07-29 11:28:06 +0300

    task31 complite

---
+++ Task31/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+bool check = false;
+int size = 0;
+int k = 0;
+
+Console.Write("Введите пятизначное число:");
+string userEnter = Console.ReadLine();
+
+check = int.TryParse(userEnter, out size);
+
+if (check)
+{
+    for (int i = 0; i < 2; i++)
+        if (userEnter[i] != userEnter[4-i])
+        {
+            k = 1;
+            break;
+        } 
+}
+else 
+{
+    Console.WriteLine("Вы ввели не число!");
+    return;
+}    
+if (k==0)
+{
+    Console.WriteLine($" {userEnter} -> да");
+}
+else
+{
+    Console.WriteLine($" {userEnter} -> нет");
+}
