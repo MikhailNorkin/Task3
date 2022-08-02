2892ce62713677f75371db6a764ec0a59dd28c85
Mikhail <mvnorkin@gmail.com>
2022-08-02 17:54:35 +0300

    Task 33 complite

---
+++ Task33/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int a = 0;
+
+void function(int a)
+{
+    Console.Write($"{a} -> ");
+    int res = 0;
+    for(int i = 1; i <= a; i++)
+    {
+        res = i*i*i;
+        Console.Write($" {res}");
+    }
+    Console.WriteLine();
+    return;
+
+}
+
+Console.Write($"Введите число: ");
+function(int.Parse(Console.ReadLine()));
